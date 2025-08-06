beea03775f51c33dcb627d3df39208e299a60c70
Nikita Lebedev <LebedevWebProgrammer@yandex.ru>
2025-08-06 20:54:37 +0300

    Create test nested navigation

--- src/app/navigators/Navigator.tsx
+++ src/app/navigators/Navigator.tsx
@@ -4,11 +4,18 @@ import BottomTabsBar from './ui/BottomTab';
 import SessionScreen from '../../screens/your-tasks';
 import TimerScreen from '../../screens/timer';
 import TaskManagerScreen from '../../screens/settings';
-import { TabParamList } from '../../@types/navigators';
+import { StackParamList, TabParamList } from '../../@types/navigators';
 import { FONT_FAMILY } from '../../shared/config/customFont';
 import { Colors } from '../../shared/styles/colorsPalete';
+import { createNativeStackNavigator, NativeStackNavigationProp } from '@react-navigation/native-stack';
+import AllTasksScreen from '../../screens/all-tasks';
+import AddNewTaskScreen from '../../screens/add-new-task';
+import { TouchableOpacity } from 'react-native';
+import Ionicons from '@react-native-vector-icons/ionicons';
+import { useNavigation } from '@react-navigation/native';
 
 
+const Stack = createNativeStackNavigator<StackParamList>();
 const Tab = createBottomTabNavigator<TabParamList>();
 
 
@@ -36,11 +43,6 @@ function PomodoroBottomTabs() {
           component={SessionScreen}
           options={{
             title: 'Your Tasks',
-            // headerLeft: () => (
-            //   <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft: 15 }}>
-            //     <Ionicons name="chevron-back" size={24} color="white" />
-            //   </TouchableOpacity>
-            // ),
           }}
 
         />
@@ -56,15 +58,62 @@ function PomodoroBottomTabs() {
           component={TaskManagerScreen}
           options={{
             title: 'Settings',
-            // headerLeft: () => (
-            //   <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft: 15 }}>
-            //     <Ionicons name="chevron-back" size={24} color="white" />
-            //   </TouchableOpacity>
-            // ),
           }}
         />
     </Tab.Navigator>
   );
 }
 
-export default PomodoroBottomTabs;
+export type YourTaskScreenNavigationProp = NativeStackNavigationProp<StackParamList, 'Tabs'>;
+
+export default function PomodoroNavigation() {
+  const navigation = useNavigation<YourTaskScreenNavigationProp>();
+
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        animation: 'fade',
+        headerTitleAlign: 'center',
+        headerShadowVisible: false,
+        headerStyle: {
+          backgroundColor: Colors.background,
+        },
+
+        headerTintColor: Colors.white,
+        
+        headerTitleStyle: {
+          fontFamily: FONT_FAMILY.AvenirNext_BOLD,
+          fontSize: 18,
+        },
+      }}
+    >
+      <Stack.Screen name="Tabs" component={PomodoroBottomTabs} 
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen 
+        name="AllTasks"
+        component={AllTasksScreen} 
+        options={{
+          title: 'All Task',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft: 15 }}>
+              <Ionicons name="chevron-back" size={24} color="white" />
+            </TouchableOpacity>
+          ),
+        }}/>
+      <Stack.Screen 
+        name="AddTask"
+        component={AddNewTaskScreen} 
+        options={{
+          title: 'Add new Task',
+          headerLeft: () => (
+            <TouchableOpacity onPress={() => navigation.goBack()} style={{ marginLeft: 15 }}>
+              <Ionicons name="chevron-back" size={24} color="white" />
+            </TouchableOpacity>
+          ),
+        }}/>
+    </Stack.Navigator>
+  );
+}
--- src/screens/your-tasks/index.tsx
+++ src/screens/your-tasks/index.tsx
@@ -3,16 +3,36 @@ import {
   View,
   Text,
   StatusBar,
-  SafeAreaView
+  SafeAreaView,
+  Button
 } from 'react-native';
 import { sceenStyle } from '../../shared/styles/screens';
+import { StackParamList } from '../../@types/navigators';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
 
+
+
+type SessionScreenNavigationProp = NativeStackNavigationProp<StackParamList, 'Tabs'>;
 
 function SessionScreen(): React.JSX.Element {
+  const navigation = useNavigation<SessionScreenNavigationProp>();
+
+  
   return (
     <SafeAreaView  style={[sceenStyle.main]}>
         <StatusBar barStyle="light-content"/>
         <Text>Экран сессий</Text>
+
+        <Button
+          title="All Tasks"
+          onPress={() => navigation.navigate('AllTasks')}
+        />
+
+        <Button
+          title="Add New Task"
+          onPress={() => navigation.navigate('AddTask')}
+        />
     </SafeAreaView>
   );
 }
